0b94f24111659f4647b95e62b02c7d2f3fc852c8
dneuterman <dneuterman@gmail.com>
2024-01-20 21:11:58 -0500

    courseinfo 2.2 complete

--- courseinfo/src/components/Course.jsx
+++ courseinfo/src/components/Course.jsx
@@ -1,10 +1,14 @@
 import Part from './Part'
 
 const Course = ({ course }) => {
+    const sum = course.parts.reduce((acc, part) => {
+        return acc + part.exercises;
+    }, 0)
     return (
         <div>
             <h1>{course.name}</h1>
             {course.parts.map(part => <Part key={part.id} part={part} />)}
+            <p>Number of exercises: {sum}</p>
         </div>
     )
 }
